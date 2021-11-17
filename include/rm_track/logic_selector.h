8cd438b5e83fa0736918137f673d718d3fcd3727
chenzheng <1047979458@11.com>
2021-11-17 16:53:15 +0800

    Add logic_selector

---
+++ include/rm_track/logic_selector.h
@@ -0,0 +1,53 @@
+//
+// Created by chenzheng on 2021/11/16.
+//
+
+#pragma once
+
+#include "buffer.h"
+#include <ros/ros.h>
+
+namespace rm_track
+{
+struct Armor
+{
+  int id;
+  tf2::Transform transform;
+};
+
+class LogicSelectorBase
+{
+public:
+  LogicSelectorBase() = default;
+  virtual void input(const Buffer buffer);
+  Armor output() const
+  {
+    return target_armor_;
+  }
+
+protected:
+  Armor last_armor_, target_armor_;
+};
+
+class SameIDArmorSelector : public LogicSelectorBase
+{
+public:
+  SameIDArmorSelector() = default;
+  void input(const Buffer buffer) override;
+};
+
+class StaticArmorSelector : public LogicSelectorBase
+{
+public:
+  StaticArmorSelector() = default;
+  void input(const Buffer buffer) override;
+};
+
+class ClosestArmorSelector : public LogicSelectorBase
+{
+public:
+  ClosestArmorSelector() = default;
+  void input(const Buffer buffer) override;
+};
+
+}  // namespace rm_track
---
+++ src/logic_selector.cpp
@@ -0,0 +1,21 @@
+//
+// Created by chenzheng on 2021/11/16.
+//
+
+#include "rm_track/logic_selector.h"
+
+namespace rm_track
+{
+void SameIDArmorSelector::input(const Buffer buffer)
+{
+}
+
+void StaticArmorSelector::input(const Buffer buffer)
+{
+}
+
+void ClosestArmorSelector::input(const Buffer buffer)
+{
+}
+
+}  // namespace rm_track
